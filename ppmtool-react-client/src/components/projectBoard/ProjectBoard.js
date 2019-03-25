74e83323c1a3eabe0e08c5c69593e6e5a9811587
John Dekis <John.Dekis@SSINA.BIZ>
2019-03-25 10:22:44 -0400

    conditional rendering for project board, project tasks, and errors

--- ppmtool-react-client/src/components/projectBoard/ProjectBoard.js
+++ ppmtool-react-client/src/components/projectBoard/ProjectBoard.js
@@ -1,75 +1,80 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import Backlog from './Backlog';
+import { getBacklog } from '../../actions/backlogActions'
+//import { getErrors } from '../../actions/'
 
 
 export class ProjectBoard extends Component {
+  constructor() {
+    super()
+    this.state = {
+      errors: {}
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.errors) {
+      this.setState({ errors: nextProps.errors})
+    }
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getBacklog(id)
+  }
+
   render() {
     const { id } = this.props.match.params;  
+    const { project_tasks } = this.props;
+    const { errors } = this.state
+
+    let BoardContent; 
+
+    const boardDisplay = (project_tasks, errors) => {
+      if(project_tasks.length < 1 ) {
+        if(errors.projectNotFound) {
+          return (
+            <div className="alert alert-danger text-center" role="alert">{errors.projectNotFound}</div>
+          )
+        } else {
+          return ( 
+            <div className="alert alert-info text-center" role="alert"> Ther are no Tasks for this project! </div>
+            )
+        }
+      } else {
+        return (<Backlog project_tasks={project_tasks} />)
+      }
+    }
+
+    BoardContent = boardDisplay(project_tasks, errors)
+    
     return (
-
-        <div className="container">
-            <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
-                <i className="fas fa-plus-circle"> Create Project Task</i>
-            </Link>
-            <br />
-            <hr />
-        
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4">
-                        <div className="card text-center mb-2">
-                            <div className="card-header bg-secondary text-white">
-                                <h3>TO DO</h3>
-                            </div>
-                        </div>
-
-            
-                    <div className="card mb-1 bg-light">
-
-                        <div className="card-header text-primary">
-                            ID: projectSequence -- Priority: priorityString
-                        </div>
-                        <div className="card-body bg-light">
-                            <h5 className="card-title">project_task.summary</h5>
-                            <p className="card-text text-truncate ">
-                                project_task.acceptanceCriteria
-                            </p>
-                            <a href="" className="btn btn-primary">
-                                View / Update
-                            </a>
-
-                            <button className="btn btn-danger ml-4">
-                                Delete
-                            </button>
-                        </div>
-                    </div>
-
-                    
-                </div>
-                <div className="col-md-4">
-                    <div className="card text-center mb-2">
-                        <div className="card-header bg-primary text-white">
-                            <h3>In Progress</h3>
-                        </div>
-                    </div>
-                    
-                </div>
-                <div className="col-md-4">
-                    <div className="card text-center mb-2">
-                        <div className="card-header bg-success text-white">
-                            <h3>Done</h3>
-                        </div>
-                    </div>
-                 
-                </div>
-            </div>
-        </div>
-
-        
-    </div>
+      
+      <div className="container">
+          <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
+              <i className="fas fa-plus-circle"> Create Project Task</i>
+          </Link>
+          <br />
+          <hr />
+         {BoardContent}
+          
+      </div>
     )
   }
 }
 
-export default connect(null)(ProjectBoard)
+ProjectBoard.propTypes = {
+  backlog : PropTypes.object.isRequired,
+  getBacklog : PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  project_tasks: state.backlog.project_tasks,
+  errors: state.errors
+})
+
+export default connect(mapStateToProps, { getBacklog })(ProjectBoard)
